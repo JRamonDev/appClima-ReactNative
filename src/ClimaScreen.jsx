9e6bc0ace6b09e8354cd3e3b316d44072c83123e
Jramongs18 <jramongs15@gmail.com>
2023-06-29 18:08:33 -0600

    Funcion ClimaScreen

--- src/ClimaScreen.jsx
+++ src/ClimaScreen.jsx
@@ -1,3 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+
+function ClimaScreen() {
+    const [climaData, setClimaData] = useState(null);
+    const countries = [
+        'Nicaragua',
+        'Costa Rica',
+        'El Salvador',
+        'Guatemala',
+        'Honduras',
+        'Panama',
+        'Belice'
+    ];
+
+    useEffect(() => {
+        const fetchClimaData = async () => {
+            try {
+                const responsePromise = countries.map(country =>
+                    axios.get(
+                        `http://api.weatherapi.com/v1/current.json?key=f41e361dff1e4b12a4a235847232906&q=${country}&lang=es`,
+                    ),
+                );
+                const responses = await Promise.all(responsePromise);
+                const climaDataArray = responses.map(response => response.data);
+                setClimaData(climaDataArray);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchClimaData();
+    }, []);
+
+    if (!climaData) {
+        return (
+            <View>
+                <Text>Cargando...</Text>
+            </View>
+        );
+    }
+}
+
+export default ClimaScreen;
+
+
