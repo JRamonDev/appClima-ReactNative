d078ea710c12f4202409120b950fd4c248b67a70
Jramongs18 <jramongs15@gmail.com>
2023-06-29 20:11:16 -0600

    Agregando estilos y retoques

--- src/ClimaScreen.jsx
+++ src/ClimaScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import axios from 'axios';
 
 function ClimaScreen() {
@@ -11,7 +11,7 @@ function ClimaScreen() {
         'Guatemala',
         'Honduras',
         'Panama',
-        'Belice'
+        'Belize'
     ];
 
     useEffect(() => {
@@ -35,28 +35,132 @@ function ClimaScreen() {
     if (!climaData) {
         return (
             <View>
-                <Text>Cargando...</Text>
+                <Text>
+                    Cargando...
+                </Text>
             </View>
         );
     }
 
     return (
-        <View>
-            {climaData.map((clima, index) => (
-                <View key={index}>
-                    <Text>
-                        Ubicacion: {clima.location.name}, {clima.location.region},{' '}
-                        {clima.location.country}
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+            <View>
+                <View
+                    style={styles.containertitle}
+                >
+                    <Text
+                        style={styles.title}
+                    >
+                        Clima de Países de Centroamérica
                     </Text>
-                    <Text>Temperatura: {clima.current.temp_c} °C</Text>
-                    <Text> {clima.current.icon} </Text>
-                    <Text>Condicion: {clima.current.condition.text}</Text>
-                    <Text>--------------------------------------------</Text>
                 </View>
-            ))}
-        </View>
+
+                {climaData.map((clima, index) => (
+                    <View
+                        style={styles.container}
+                        key={index}
+                    >
+                        <Text
+                            style={styles.descriptionextra}
+                        >
+                            -----------------------------------------------------------------------
+                        </Text>
+                        <Text
+                            style={styles.descriptionlocation}
+                        >
+                            {clima.location.name}
+                            {', '}
+                            {clima.location.country}
+                        </Text>
+
+                        <Text
+                            style={styles.descriptiontemp}
+                        >
+                            {clima.current.temp_c} °C
+                            {'     |     '}
+                            {clima.current.temp_f} °F
+                        </Text>
+
+                        <Text
+                            style={styles.descriptioncondition}
+                        >
+                            {clima.current.condition.text}
+                        </Text>
+
+                        <Text
+                            style={styles.descriptionextra}
+                        >
+                            Viento:
+                            {' '}
+                            {clima.current.wind_mph}
+                            {'    '}
+                            Presión:
+                            {' '}
+                            {clima.current.pressure_in}
+                            {'    '}
+                            Humedad:
+                            {' '}
+                            {clima.current.humidity}
+
+
+                        </Text>
+                        <Text
+                            style={styles.descriptionextra}
+                        >
+                            -----------------------------------------------
+                        </Text>
+
+                    </View>
+                ))}
+            </View>
+        </ScrollView >
     );
 }
+
+const styles = StyleSheet.create({
+    containertitle: {
+        marginTop: 10,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#332F2C',
+
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 24,
+        backgroundColor: '#332F2C',
+        borderRadius: 10,
+        marginTop: 20,
+        marginLeft: 15,
+        marginRight: 15
+    },
+    descriptionlocation: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    descriptiontemp: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#61DBFB',
+    },
+    descriptioncondition: {
+        fontSize: 15,
+        fontStyle: 'italic',
+        color: 'skyblue',
+    },
+    descriptionextra: {
+        fontSize: 16,
+        color: 'gray',
+    },
+});
 
 export default ClimaScreen;
 
